267e3aca00bdce31c280376b866f33a86450b4e7
Hong-Jun-Il <86016726+Hong-Jun-Il@users.noreply.github.com>
2025-08-14 13:13:45 +0900

    professor names json > varchar로 변경, 검색 색인 추가(현재는 like로 검색)

--- sql/ddl/04_create_course_table.sql
+++ sql/ddl/04_create_course_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE course (
     course_id VARCHAR(255) PRIMARY KEY,
     course_code VARCHAR(255) NOT NULL,
     course_name VARCHAR(255) NOT NULL,
-    professor_names JSON NOT NULL,
+    professor_names VARCHAR(255) NOT NULL,
     delivery_method VARCHAR(255) NOT NULL,
     credit INT NOT NULL,
     day_or_night ENUM('day', 'night', 'both') NOT NULL,
@@ -11,5 +11,6 @@ CREATE TABLE course (
     grade_limit INT,
     online_hour DECIMAL(2,1) NOT NULL DEFAULT 0.0,
     plan_code VARCHAR(255),
+    FULLTEXT(course_code, course_name, professor_names),
     FOREIGN KEY (semester_id) REFERENCES semester(semester_id) ON DELETE CASCADE
 );
